2566ea5d193c20272b35f3626fcb78f677670860
Martinskatvedt <martinskatvedt@gmail.com>
2022-03-02 13:58:13 +0100

    added backend with custom user

---
+++ backend/core/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "owID",
+        "nickname",
+        "family_name",
+        "is_staff",
+    )
+
+
+admin.site.register(User, UserAdmin)
---
+++ backend/core/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserProfileManager(BaseUserManager):
+    # Create a new user profile
+    def create_user(self, nickname, password, owID):
+
+        if not owID:
+            raise ValueError("Must have an owID")
+
+        user = self.model(nickname=nickname, owID=owID)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    # Create a new superuser profile
+    def create_superuser(self, nickname, password, owID):
+
+        user = self.create_user(nickname, password, owID)
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+# Database model for users in the system
+class User(AbstractBaseUser, PermissionsMixin):
+
+    # Fields for the user model
+    owID = models.IntegerField(primary_key=True)
+    family_name = models.CharField(max_length=255, blank=True, null=True)
+    given_name = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    nickname = models.CharField(max_length=255, unique=True)
+    picture = models.CharField(max_length=1000, blank=True, null=True)
+    preferred_username = models.CharField(max_length=255, blank=True, null=True)    
+    is_staff = models.BooleanField(default=False)
+
+    points = models.IntegerField(default=0)
+    flags = models.IntegerField(default=0)
+
+
+
+    # Usermanager for creating users
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = "nickname"
+    REQUIRED_FIELDS = ["owID"]
+
+    def __str__(self):
+        return self.nickname
+
